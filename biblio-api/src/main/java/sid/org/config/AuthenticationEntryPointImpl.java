f609f9501874cb3b9cea0a1ce8b66c335d1d7e31
guillaume alisse <gualisse@gmail.com>
2020-05-19 15:47:59 +0200

    api securise2

--- biblio-api/src/main/java/sid/org/config/AuthenticationEntryPointImpl.java
+++ biblio-api/src/main/java/sid/org/config/AuthenticationEntryPointImpl.java
@@ -4,7 +4,7 @@ package sid.org.config;
 import java.io.IOException;
 import java.io.PrintWriter;
  
-import javax.servlet.ServletException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
  
