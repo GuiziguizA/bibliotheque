e04f14b49d1ca10b8071887f26e13f2105c8761c
guillaume alisse <gualisse@gmail.com>
2020-05-19 10:31:14 +0200

    api securisé

--- biblio-api/src/main/java/sid/org/config/ApiSecurityConfig.java
+++ biblio-api/src/main/java/sid/org/config/ApiSecurityConfig.java
@@ -2,10 +2,18 @@ package sid.org.config;
 
 
   
-  import org.springframework.context.annotation.Bean; import
-  org.springframework.context.annotation.Configuration; import
-  org.springframework.http.HttpMethod; import
-  org.springframework.security.authentication.AuthenticationManager; import
+  import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import
   org.springframework.security.config.annotation.method.configuration.
   EnableGlobalMethodSecurity; import
   org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +21,13 @@ package sid.org.config;
   EnableWebSecurity; import
   org.springframework.security.config.annotation.web.configuration.
   WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
   
   
   
@@ -23,24 +38,37 @@ package sid.org.config;
   
   @EnableWebSecurity
   
-  @EnableGlobalMethodSecurity(securedEnabled = true) public class
-  ApiSecurityConfig extends WebSecurityConfigurerAdapter {
+  @EnableGlobalMethodSecurity(securedEnabled = true) 
+  public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
   
  /**
 	 * configuration de spring security
+	 * 
+	 * 
+	 * 
 	 */
+	  @Autowired
+	  CustomUserDetailService userDetailsService;
+	  @Autowired
+	    private AuthenticationEntryPoint authEntryPoint;
+	    
+	    @Override
+	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.userDetailsService(userDetailsService);
+	    }
+
 		  @Override protected void configure(HttpSecurity http) throws Exception {
 			  http
-			  .csrf().disable()
-		  .authorizeRequests()
-		  .antMatchers("/css/**", "/js/**", "/webjars/**", "/books","/books/**", "/prets/**","/users", "/users/**" ).permitAll()
-		  .anyRequest().authenticated() 
-		
-				;
+			  .csrf().disable();
+			  // All requests send to the Web Server request must be authenticated
+		        http.authorizeRequests().anyRequest().authenticated();
+		 
+		        // Use AuthenticationEntryPoint to authenticate user/password
+		        http.httpBasic().authenticationEntryPoint(authEntryPoint);
+              
 		  
 		  }
 		  
-		  
-		  
+		
 		  }
 		 
